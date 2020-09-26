740a3727dab600a86d4fedf17540c35e5c21131c
Ramazan Öğünç <ramazanogunc@hotmail.com>
2020-09-26 10:58:35 +0300

    Move rewarded ads from donate fragment to main activity (drawer navigation)

--- app/src/main/java/com/ramo/quran/ui/MainActivity.kt
+++ app/src/main/java/com/ramo/quran/ui/MainActivity.kt
@@ -1,29 +1,51 @@
 package com.ramo.quran.ui
 
 import android.content.Context
-import android.content.res.Configuration
-import android.content.res.Resources
 import android.os.Bundle
-import android.util.DisplayMetrics
-import android.util.Log
 import android.view.MenuItem
+import androidx.annotation.NonNull
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
 import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.onNavDestinationSelected
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.gms.ads.AdError
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.LoadAdError
 import com.google.android.gms.ads.MobileAds
+import com.google.android.gms.ads.rewarded.RewardItem
+import com.google.android.gms.ads.rewarded.RewardedAd
+import com.google.android.gms.ads.rewarded.RewardedAdCallback
+import com.google.android.gms.ads.rewarded.RewardedAdLoadCallback
 import com.google.android.material.navigation.NavigationView
 import com.ramo.quran.R
 import com.ramo.quran.dataAccess.LocalSqliteHelper
 import com.ramo.quran.dataAccess.abstr.SqliteResponse
-import com.ramo.quran.helper.*
+import com.ramo.quran.helper.LocaleWrapper
+import com.ramo.quran.helper.setProgressDialog
+import com.ramo.quran.helper.showError
+import com.ramo.quran.helper.showInfo
 import com.ramo.quran.model.Config
 import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
+
+    private val rewardedAd: RewardedAd by lazy {
+        RewardedAd(this, "ca-app-pub-3405727563403529/5085998150")
+    }
+
+    private val adCallback = object : RewardedAdCallback() {
+        override fun onRewardedAdOpened() {}
+        override fun onRewardedAdClosed() {}
+        override fun onUserEarnedReward(@NonNull reward: RewardItem) {
+            showInfo(getString(R.string.thanks_for_watch_ads))
+        }
+
+        override fun onRewardedAdFailedToShow(adError: AdError) {}
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,15 +53,42 @@ class MainActivity : AppCompatActivity() {
         MobileAds.initialize(this) {} // init ads
         setSupportActionBar(toolbar)
 
-        initNavigationComponenet()
+        initNavigationComponent()
     }
 
-    private fun initNavigationComponenet() {
+    private fun initNavigationComponent() {
         val navController = findNavController(R.id.navHostFragment)
         findViewById<NavigationView>(R.id.nav_view)
             .setupWithNavController(navController)
         // toolbar
-        findViewById<Toolbar>(R.id.toolbar).setupWithNavController(navController,drawerLayout)
+        findViewById<Toolbar>(R.id.toolbar).setupWithNavController(navController, drawerLayout)
+        // handle watch ads menu item
+        nav_view.setNavigationItemSelectedListener {
+            drawerLayout.close()
+            if (it.itemId == R.id.watch_ads) {
+                showAds()
+                false
+            } else {
+                NavigationUI.onNavDestinationSelected(it, navController)
+                true
+            }
+        }
+    }
+
+    private fun showAds() {
+        val dialog = setProgressDialog(this, "Ads is loading. Please wait")
+        dialog.show()
+        rewardedAd.loadAd(AdRequest.Builder().build(), object : RewardedAdLoadCallback() {
+            override fun onRewardedAdLoaded() {
+                dialog.dismiss()
+                rewardedAd.show(this@MainActivity, adCallback)
+            }
+
+            override fun onRewardedAdFailedToLoad(adError: LoadAdError) {
+                dialog.dismiss()
+                showError()
+            }
+        })
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -51,17 +100,18 @@ class MainActivity : AppCompatActivity() {
         var lang = "en"
         LocalSqliteHelper(newBase).getAllConfig(object : SqliteResponse<Config> {
             override fun onSuccess(response: Config) {
-                lang = if (response.language.id == 1 )"tr" else "en"
+                lang = if (response.language.id == 1) "tr" else "en"
             }
+
             override fun onFail(failMessage: String) {
-                TODO("Not yet implemented")
+
             }
         })
-        super.attachBaseContext(LocaleWrapper.wrap(newBase!!,lang))
+        super.attachBaseContext(LocaleWrapper.wrap(newBase!!, lang))
     }
 
     override fun onBackPressed() {
-        if(drawerLayout.isDrawerOpen(GravityCompat.START))
+        if (drawerLayout.isDrawerOpen(GravityCompat.START))
             drawerLayout.close()
         else
             super.onBackPressed()
--- app/src/main/java/com/ramo/quran/ui/fragment/DonateFragment.kt
+++ app/src/main/java/com/ramo/quran/ui/fragment/DonateFragment.kt
@@ -1,103 +1,19 @@
 package com.ramo.quran.ui.fragment
 
-import android.app.Activity
-import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import androidx.annotation.NonNull
 import androidx.fragment.app.Fragment
-import com.google.android.gms.ads.AdError
-import com.google.android.gms.ads.AdRequest
-import com.google.android.gms.ads.LoadAdError
-import com.google.android.gms.ads.rewarded.RewardItem
-import com.google.android.gms.ads.rewarded.RewardedAd
-import com.google.android.gms.ads.rewarded.RewardedAdCallback
-import com.google.android.gms.ads.rewarded.RewardedAdLoadCallback
 import com.ramo.quran.R
-import com.ramo.quran.helper.showError
-import com.ramo.quran.helper.showSuccess
 
 
-class DonateFragment: Fragment() {
-
-    private lateinit var rewardedAd: RewardedAd
-
+class DonateFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_donate, container, false)
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        view.findViewById<Button>(R.id.buttonWatchAds).setOnClickListener { onWatchAdsClick() }
-
-        activity?.let {
-            rewardedAd = initRewardAds(it);
-        }
-
-    }
-
-    private fun onWatchAdsClick() {
-        rewardedAd?.let {
-            if (it.isLoaded) {
-                activity?.let {
-                    rewardedAd.show(it, adCallback)
-                    loadAds(rewardedAd,adLoadCallback)
-                }
-            }
-            else {
-                loadAds(rewardedAd,adLoadAndShowCallback)
-            }
-        }
-    }
-
-    private fun initRewardAds(activity: Activity): RewardedAd {
-        return RewardedAd(activity, "ca-app-pub-3405727563403529/5085998150")
-    }
-
-    private fun loadAds(rewardedAd: RewardedAd, adLoadCallback: RewardedAdLoadCallback){
-        rewardedAd.loadAd(AdRequest.Builder().build(), adLoadCallback)
-    }
-
-    private val adLoadCallback = object: RewardedAdLoadCallback() {
-        override fun onRewardedAdLoaded() {
-        }
-        override fun onRewardedAdFailedToLoad(adError: LoadAdError) {
-        }
-    }
-
-    private val adLoadAndShowCallback = object: RewardedAdLoadCallback() {
-        override fun onRewardedAdLoaded() {
-            rewardedAd?.let(fun(ads: RewardedAd) {
-                activity?.let {
-                    ads.show(it,adCallback)
-                }
-            })
-        }
-        override fun onRewardedAdFailedToLoad(adError: LoadAdError) {
-            activity?.showError()
-        }
-    }
-
-    private val adCallback = object: RewardedAdCallback() {
-        override fun onRewardedAdOpened() {
-        }
-        override fun onRewardedAdClosed() {
-            loadAds(rewardedAd,adLoadCallback)
-        }
-        override fun onUserEarnedReward(@NonNull reward: RewardItem) {
-            activity?.showSuccess()
-            activity?.let { loadAds(rewardedAd ,adLoadCallback) }
-        }
-        override fun onRewardedAdFailedToShow(adError: AdError) {
-            loadAds(rewardedAd,adLoadCallback)
-        }
     }
 }
