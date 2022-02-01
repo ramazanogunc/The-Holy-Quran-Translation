4772a50a6b328ad1d774bcc843016f54a70fcb4c
Ramazan Öğünç <ramazanogunc@hotmail.com>
2022-02-01 20:43:11 +0300

    Refactor first open works

--- app/src/main/java/com/ramo/quran/MyApplication.kt
+++ app/src/main/java/com/ramo/quran/MyApplication.kt
@@ -1,30 +1,14 @@
 package com.ramo.quran
 
 import android.app.Application
-import com.ramo.quran.data.AppDatabase
-import com.ramo.quran.data.shared_pref.AppSharedPref
-import com.ramo.quran.utils.LocaleHelper
 import com.yariksoffice.lingver.Lingver
 import dagger.hilt.android.HiltAndroidApp
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import java.util.*
 
 @HiltAndroidApp
 class MyApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        val pref = AppSharedPref(this)
-        if (pref.isFirstLogin) {
-            val localeId = LocaleHelper().getLocaleId(Locale.getDefault().language)
-            GlobalScope.launch {
-                AppDatabase.getDatabase(this@MyApplication).configDao.changeLocaleWithResource(
-                    localeId
-                )
-            }
-            pref.isFirstLogin = false
-        }
         Lingver.init(this)
     }
 }
--- app/src/main/java/com/ramo/quran/ui/splash_activity/SplashActivity.kt
+++ app/src/main/java/com/ramo/quran/ui/splash_activity/SplashActivity.kt
@@ -1,21 +1,28 @@
-package com.ramo.quran.ui
+package com.ramo.quran.ui.splash_activity
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.ramo.quran.data.shared_pref.AppSharedPref
+import com.ramo.quran.ui.MainActivity
 import com.ramo.quran.utils.ThemeHelper
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
+@SuppressLint("CustomSplashScreen")
 @AndroidEntryPoint
 class SplashActivity : AppCompatActivity() {
+
+    val viewModel by viewModels<SplashViewModel>()
 
     @Inject
     lateinit var pref: AppSharedPref
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        viewModel.detectLanguageInFirstOpen()
         ThemeHelper.setThemeMode(pref.appTheme)
 
         val intent = Intent(this@SplashActivity, MainActivity::class.java)
